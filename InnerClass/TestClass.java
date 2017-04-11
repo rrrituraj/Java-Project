83eade23ba2343f8ae27cc19575255c17e4aeada
Rituraj Gupta <rrrituraj@users.noreply.github.com>
2017-04-11 21:54:27 +0530

    practise

--- InnerClass/TestClass.java
+++ InnerClass/TestClass.java
@@ -5,9 +5,14 @@ package com.tcs.InnerClass;
  */
 public class TestClass {
     public static void main(String[] args) {
-        Outer1 o=new Outer1();
+       /* Outer1 o=new Outer1();
         Outer1.Inner1 i=o.new Inner1();
         i.m1();
-        o.m2();
+        o.m2();*/
+
+       A a=new A();
+       A.B b=a.new B();
+       A.B.C c=b.new C();
+       c.m1();
     }
 }
